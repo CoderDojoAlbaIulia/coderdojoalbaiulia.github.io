fcfa9fe1ab69945d5f9fdcb38d1ac9130f608b10
Razvan <razvan@whitecitycode.com>
2024-01-24 20:27:13 +0200

    fix: add locale translation for map messages

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -16,6 +16,13 @@ export default function MyMap() {
       style={{ width: 600, height: 600 }}
       mapStyle="mapbox://styles/razvan42/clrj101eb00oo01qththo326l"
       cooperativeGestures={true}
+      locale={{
+        "ScrollZoomBlocker.CtrlMessage":
+          "Folosește CTRL + Scroll pentru a mări harta ",
+        "ScrollZoomBlocker.CmdMessage":
+          "Folosește ⌘ + scroll pentru a mări harta",
+        "TouchPanBlocker.Message": "Folosește 2 degete pentru a mări harta",
+      }}
     >
       <Marker longitude={longitude} latitude={latitude} anchor="bottom">
         <a href="https://www.google.com/maps/place/Resource+Center+-+Ladies+Academy/@46.0709601,23.5448508,15z/data=!4m6!3m5!1s0x474ea7d08f406dc7:0x7cef99f43db6a8c4!8m2!3d46.0695681!4d23.5558827!16s%2Fg%2F11c6cdh7t4?entry=tts">
